313e0f39bbaa06132dcb653a107ea58ac8e21e02
Randy Lagdaan <rdlagdaan@gmail.com>
2018-01-29 09:29:43 -0800

    Baseline

--- config/database.js
+++ config/database.js
@@ -1,8 +1,8 @@
 // Asynchronous
-const crypto = require('crypto').randomBytes(256).toString('hex');
+const crypto = require('crypto').randomBytes(256).toString('hex'); // Provides cryptographic functionality (OpenSSL's hash, HMAC, cipher, decipher, sign and verify functions)
 
 module.exports = {
-    uri: 'mongodb://localhost:27017/' + this.db,
-    secret: crypto,
-    db: 'mean-angular-2'
+    uri: 'mongodb://localhost:27017/meandb', // Databse URI and database name
+    secret: crypto, // Cryto-created secret
+    db: 'meandb' // Database name
 }
